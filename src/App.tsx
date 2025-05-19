3d4be612a0b8605db360c0c5646056815c5c24a8
Alejandro <aleangrodriguez123@gmail.com>
2025-05-19 18:17:23 -0500

    feat: add react-router and update App UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./index.css";
-
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
