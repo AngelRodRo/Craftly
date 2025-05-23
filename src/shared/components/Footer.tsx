c79d2cdfec0d0bbb9535f46d10f15cccf18e737a
Alejandro <aleangrodriguez123@gmail.com>
2025-05-22 20:54:47 -0500

    feat: add footer component

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,3 +1,11 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex justify-between items-center p-4 bg-white border-t border-gray-200 fixed bottom-0 w-full z-10">
+      <div className="flex items-center gap-2 w-full justify-center">
+        <p className="text-sm text-gray-500 text-center">
+          &copy; {new Date().getFullYear()} Craftly. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
