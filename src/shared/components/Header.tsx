0298c2dd88af2edc60917200be56f2be43dd232f
Alejandro <aleangrodriguez123@gmail.com>
2025-05-22 20:55:04 -0500

    feat: add header component

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,3 +1,19 @@
+import { ShoppingCartIcon } from "lucide-react";
+import { Link } from "react-router";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <nav className="flex justify-between items-center p-4 bg-white border-b border-gray-200 fixed top-0 w-full z-10">
+      <div className="flex items-center gap-2 justify-center">
+        <Link to="/">
+          <h1 className="text-2xl font-bold">Craftly</h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-2 justify-end">
+        <Link to="/cart">
+          <ShoppingCartIcon className="w-6 h-6" />
+        </Link>
+      </div>
+    </nav>
+  );
 }
