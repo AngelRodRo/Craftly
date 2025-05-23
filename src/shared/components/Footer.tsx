f3cd2da898393915c28fe6161b5d7e15152049c4
Alejandro <aleangrodriguez123@gmail.com>
2025-05-22 21:18:48 -0500

    refactor: remove fixed position for header and footer

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-between items-center p-4 bg-white border-t border-gray-200 fixed bottom-0 w-full z-10">
+    <footer className="flex justify-between items-center p-4 bg-white border-t border-gray-200 w-full z-10">
       <div className="flex items-center gap-2 w-full justify-center">
         <p className="text-sm text-gray-500 text-center">
           &copy; {new Date().getFullYear()} Craftly. All rights reserved.
