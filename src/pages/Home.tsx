30dd4168061c143f093707c1964a2624134b64b1
Alejandro <aleangrodriguez123@gmail.com>
2025-05-24 15:17:55 -0500

    feat: render basic service list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,41 @@
-import { useSelector } from "react-redux";
-import type { RootState } from "../app/store";
+import { useAppDispatch, useAppSelector } from "@/app/hook";
+import { useEffect } from "react";
+import { fetchServices } from "@/features/Services/servicesSlice";
+import { Card, CardContent } from "@/shared/components/ui/card";
 
 export default function Home() {
-  const user = useSelector((state: RootState) => state.auth.user);
+  const dispatch = useAppDispatch();
+  const services = useAppSelector((state) => state.services.services);
+
+  useEffect(() => {
+    dispatch(fetchServices());
+  }, [dispatch]);
+
   return (
     <div>
-      <h1 className="text-4xl font-bold">Hello World {user?.name}</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {services.map((service) => (
+          <Card key={service.id}>
+            <CardContent className="grid grid-cols-2 gap-4">
+              <div>
+                <img
+                  src={service.image}
+                  alt={service.name}
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-bold">{service.name}</h3>
+                <p className="text-xs text-gray-500">{service.category}</p>
+                <p className="text-sm text-gray-500">{service.description}</p>
+                <p className="text-xl font-bold">S/. {service.price}</p>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <div></div>
     </div>
   );
 }
