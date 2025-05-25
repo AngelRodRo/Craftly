13e1ee7f33cc41a16e7ab963014e79b8fff6b2f8
Alejandro <aleangrodriguez123@gmail.com>
2025-05-25 12:49:51 -0500

    refactor: create new home page with isolated filters component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,60 @@
+import { useAppDispatch, useAppSelector } from "@/app/hook";
+import { useEffect, useState } from "react";
+import {
+  fetchCategories,
+  fetchServices,
+} from "@/features/Services/servicesSlice";
+import { Card, CardContent } from "@/shared/components/ui/card";
+import { Button } from "@/shared/components/ui/button";
+import { Input } from "@/shared/components/ui/input";
+
+import Filters from "./Filters";
+
+export default function Home() {
+  const dispatch = useAppDispatch();
+
+  const services = useAppSelector((state) => state.services.services);
+  const filter = useAppSelector((state) => state.services.filter);
+
+  useEffect(() => {
+    dispatch(fetchServices(filter));
+  }, [dispatch, filter]);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  return (
+    <div className="flex flex-col gap-4 my-4">
+      <div className="flex justify-center gap-2">
+        <Input type="text" placeholder="Search" className="w-1/2" />
+        <Button>Search</Button>
+      </div>
+      <div className="flex gap-4 mx-8">
+        <Filters />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {services.map((service) => (
+            <Card key={service.id}>
+              <CardContent className="grid grid-cols-2 gap-4">
+                <div>
+                  <img
+                    src={service.image}
+                    alt={service.name}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h3 className="text-lg font-bold">{service.name}</h3>
+                  <p className="text-xs text-gray-500">{service.category}</p>
+                  <p className="text-sm text-gray-500">{service.description}</p>
+                  <p className="text-xl font-bold">S/. {service.price}</p>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
