68785dcffe7edaeec7f79603c62b5360baebaefd
Alejandro <aleangrodriguez123@gmail.com>
2025-05-30 08:51:34 -0500

    refactor: create service card

---
+++ src/features/Services/components/ServiceCard.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/shared/components/ui/button";
+import { CardContent } from "@/shared/components/ui/card";
+import { Card } from "@/shared/components/ui/card";
+import type { Service } from "../model";
+import { ShoppingCart } from "lucide-react";
+
+interface ServiceCardProps {
+  service: Service;
+}
+
+export default function ServiceCard({ service }: ServiceCardProps) {
+  return (
+    <Card key={service.id}>
+      <CardContent className="grid grid-cols-2 gap-4 items-center">
+        <div>
+          <img
+            className="rounded-lg w-full h-full object-cover"
+            src={service.image}
+            alt={service.name}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-bold">{service.name}</h3>
+          <p className="text-xs text-gray-500">{service.category}</p>
+          <p className="text-sm text-gray-500">{service.description}</p>
+          <p className="text-xl font-bold">S/. {service.price}</p>
+        </div>
+        <Button className="w-full cursor-pointer">
+          <ShoppingCart />
+          Add to cart
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,11 +4,12 @@ import {
   fetchCategories,
   fetchServices,
 } from "@/features/Services/servicesSlice";
-import { Card, CardContent } from "@/shared/components/ui/card";
+
 import { Button } from "@/shared/components/ui/button";
 import { Input } from "@/shared/components/ui/input";
 
 import Filters from "./Filters";
+import ServiceCard from "@/features/Services/components/ServiceCard";
 
 export default function Home() {
   const dispatch = useAppDispatch();
@@ -46,28 +47,11 @@ export default function Home() {
           Search
         </Button>
       </div>
-      <div className="flex gap-4 mx-8">
+      <div className="flex md:flex-row flex-col gap-4 mx-8">
         <Filters />
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {services.map((service) => (
-            <Card key={service.id}>
-              <CardContent className="grid grid-cols-2 gap-4">
-                <div>
-                  <img
-                    src={service.image}
-                    alt={service.name}
-                    width={100}
-                    height={100}
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <h3 className="text-lg font-bold">{service.name}</h3>
-                  <p className="text-xs text-gray-500">{service.category}</p>
-                  <p className="text-sm text-gray-500">{service.description}</p>
-                  <p className="text-xl font-bold">S/. {service.price}</p>
-                </div>
-              </CardContent>
-            </Card>
+            <ServiceCard key={service.id} service={service} />
           ))}
         </div>
       </div>
