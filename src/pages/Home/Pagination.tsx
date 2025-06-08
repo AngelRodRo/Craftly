329227b270f52dd9b2a0c6954439586f840cad6d
Alejandro <aleangrodriguez123@gmail.com>
2025-06-08 08:50:18 -0500

    fix: show clickeable link for pagination items

--- src/pages/Home/Pagination.tsx
+++ src/pages/Home/Pagination.tsx
@@ -18,19 +18,31 @@ export default function PaginationComponent({
   currentPage,
   onPageChange,
 }: PaginationComponentProps) {
+  const handlePageChange = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    page: number
+  ) => {
+    e.preventDefault();
+    onPageChange(page);
+  };
+
   return (
     <Pagination data-testid="pagination" className="flex justify-center">
       <PaginationContent>
         {currentPage > 1 && (
           <PaginationItem data-testid="pagination-previous">
-            <PaginationPrevious onClick={() => onPageChange(currentPage - 1)} />
+            <PaginationPrevious
+              href="#"
+              onClick={(e) => handlePageChange(e, currentPage - 1)}
+            />
           </PaginationItem>
         )}
         {Array.from({ length: totalPages }).map((_, index) => (
           <PaginationItem key={index} data-testid="pagination-item">
             <PaginationLink
+              href="#"
               isActive={currentPage === index + 1}
-              onClick={() => onPageChange(index + 1)}
+              onClick={(e) => handlePageChange(e, index + 1)}
             >
               {index + 1}
             </PaginationLink>
@@ -39,7 +51,10 @@ export default function PaginationComponent({
 
         {currentPage < totalPages && (
           <PaginationItem data-testid="pagination-next">
-            <PaginationNext onClick={() => onPageChange(currentPage + 1)} />
+            <PaginationNext
+              href="#"
+              onClick={(e) => handlePageChange(e, currentPage + 1)}
+            />
           </PaginationItem>
         )}
       </PaginationContent>
