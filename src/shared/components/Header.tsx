984b20bde61ffd3b86b31c613379ad461d6bb643
Alejandro <aleangrodriguez123@gmail.com>
2025-07-23 23:49:26 -0500

    chore: update title

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <nav className="flex justify-between items-center p-4 bg-white border-b border-gray-200 w-full z-10">
       <div className="flex items-center gap-2 justify-center">
         <Link to="/">
-          <h1 className="text-2xl font-bold">Craftly</h1>
+          <h1 className="text-2xl font-bold">Craftly Market</h1>
         </Link>
       </div>
       <div className="flex items-center gap-2 justify-end">
